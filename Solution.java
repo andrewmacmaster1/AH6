ec5770a8e2a190cad2c014d45f189d2546915ea6
MacMaster <andrew.macmaster@fiserv.com>
2023-11-01 15:29:32 -0400

    AH6

--- Solution.java
+++ Solution.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.transform.Source;
@@ -13,12 +14,18 @@ class Node<T> {
 }
 
 class Solution {
-  public static List<String> linkedListValues(Node<String> head) {
-    // todo
-    return;
+  public static ArrayList<String> linkedListValues(Node<String> head) {
+    Node<String> node = head;
+    ArrayList<String> nodeList = new ArrayList<>();
+    do {
+      nodeList.add(node.val);
+      node = node.next;
+    }
+    while (node != null);
+    return nodeList;
   }
 
-  public static void main() {
+  public static void main(String[] args) {
     Node<String> a = new Node<>("a");
     Node<String> b = new Node<>("b");
     Node<String> c = new Node<>("c");
@@ -29,7 +36,19 @@ class Solution {
 
     // a -> b -> c -> d
 
-    Solution.linkedListValues(a); 
+    System.out.println(Solution.linkedListValues(a));
     // -> [ "a", "b", "c", "d" ]
+
+    Node<String> one = new Node<>("1");
+    Node<String> two = new Node<>("2");
+    Node<String> three = new Node<>("3");
+    Node<String> four = new Node<>("4");
+    Node<String> five = new Node<>("5");
+    one.next = two;
+    two.next = three;
+    three.next = four;
+    four.next = five;
+
+    System.out.println(Solution.linkedListValues(one));
   }
 }
